f71830d4a6cfc725ce8f922d078e38d426e65be5
Christian <christian.landaverde@centre.edu>
2023-02-05 15:48:44 -0500

    first commit

---
+++ src/main/java/sayHi/UnderAgeException.java
@@ -0,0 +1,25 @@
+package sayHi;
+
+public class UnderAgeException extends Exception
+{
+	private static final long serialVersionUID = -7990395458354141240L;
+
+	/**
+	 * 
+	 */
+	public UnderAgeException()
+	{
+		super("Parent is too young");
+		
+	}
+
+	/**
+	 * 
+	 */
+	public UnderAgeException(String message)
+	{
+		super(message);
+	}
+	
+
+}
